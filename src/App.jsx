0e508d28935f80f720d6d5213e8df76731de84aa
MoriahSWalker git config --global user.email riahrosebuds@gmail.com <you@example.com>
2023-10-13 17:09:23 -0400

    start landing page css

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='App'>
+      <div className='left-side-content'>
+        <h1 id='title'>Recipe Box</h1>
+        <h2 id='tagline'> Unleash your <span id='light_text'>inner </span>chef</h2>
+        <p id='landing_p'>Find recipes for the food you love and flavors you didn't know existed!</p>
+        <div className='input-wrapper'><input type='text' placeholder='Search by ingredient or dish name' className='search-input'></input>
+          <button className='search-button'>Search</button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <div className='right-side-content'><img src='/assets/mainPicture.jpg' className='image'></img></div>
+    </div>
   )
 }
 
