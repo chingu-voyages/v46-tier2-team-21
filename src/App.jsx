99f87dd5467a013b51ad0d1fbfc377bd026a19be
MoriahSWalker git config --global user.email riahrosebuds@gmail.com <you@example.com>
2023-10-14 10:26:54 -0400

    finish landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import chefHat from './assets/chefhat.svg'
 
 function App() {
 
@@ -7,10 +8,18 @@ function App() {
     <div className='App'>
       <div className='left-side-content'>
         <h1 id='title'>Recipe Box</h1>
-        <h2 id='tagline'> Unleash your <span id='light_text'>inner </span>chef</h2>
+
+        <div className='tagline-wrapper'>
+          {/* tagline */}
+          <p id='tagline'> Unleash your <span id='light_text'> inner </span> chef </p>
+
+          {/* svg chefhat image */}
+          <span><img src={chefHat}></img></span>
+        </div>
+
         <p id='landing_p'>Find recipes for the food you love and flavors you didn't know existed!</p>
         <div className='input-wrapper'><input type='text' placeholder='Search by ingredient or dish name' className='search-input'></input>
-          <button className='search-button'>Search</button>
+          <button className='search-button' >Search</button>
         </div>
       </div>
       <div className='right-side-content'><img src='/assets/mainPicture.jpg' className='image'></img></div>
