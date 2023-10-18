bdd6bd8671876832dde9c563fc904143e9c626cd
GregNicholas <gregschoenberg@gmail.com>
2023-10-17 20:40:56 -0700

    set up search text state and form submit

--- src/components/search/search.jsx
+++ src/components/search/search.jsx
@@ -1,11 +1,25 @@
+import { useState } from 'react'
 import './style.css'
 
 const Search = () => {
-    return (
-      <div className='input-wrapper'><input type='text' placeholder='Search by ingredient or dish name' className='search-input'></input>
+  const [searchText, setSearchText] = useState('')
+
+  function updateInput(e) {
+    setSearchText(e.target.value)
+  }
+
+  function getRecipes(e) {
+    e.preventDefault()
+    console.log("find recipes with ", searchText)
+  }
+
+  return (
+    <form onSubmit={getRecipes}>
+      <div className='input-wrapper'><input onChange={updateInput} type='text' placeholder='Search by ingredient or dish name' className='search-input' value={searchText}></input>
         <button className='search-button' >Search</button>
       </div>
-    )
+    </form>
+  )
 }
 
 export default Search
