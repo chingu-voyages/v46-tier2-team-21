a6c78bd10a6841ae8509089a2c24c993414201b1
MoriahSWalker git config --global user.email riahrosebuds@gmail.com <you@example.com>
2023-10-20 16:14:19 -0400

    made edit to search input ui and add card component

---
+++ src/components/searchbar/searchbar.jsx
@@ -0,0 +1,20 @@
+import "./searchbar.css"
+
+
+
+const SearchBar = () => {
+
+
+
+    return (
+        <div className='input-wrapper'>
+            <form>
+                <input type='text' placeholder='Search by ingredient or dish name' className='search-input'>
+                </input>
+                <button className='search-button' >Search</button>
+            </form>
+        </div>
+    )
+}
+
+export default SearchBar
