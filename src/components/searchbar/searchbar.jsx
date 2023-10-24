cfb08cb2b7f8a3a179738081bb3f3fd5247a26e8
GregNicholas <gregschoenberg@gmail.com>
2023-10-23 21:54:04 -0700

    perform API fetch in SearchBar component

--- src/components/searchbar/searchbar.jsx
+++ src/components/searchbar/searchbar.jsx
@@ -1,16 +1,42 @@
+import { useState } from 'react'
 import "./searchbar.css"
 
+const SearchBar = ({ setSearchResults }) => {
+    const [searchText, setSearchText] = useState('')
 
+    const rapidApiKey = import.meta.env.VITE_RAPIDAPI_KEY
+    const rapidApiHost = import.meta.env.VITE_RAPIDAPI_HOST
 
-const SearchBar = () => {
+    function updateInput(e) {
+        setSearchText(e.target.value)
+    }
 
-
+    async function getRecipes(e) {
+        e.preventDefault()
+        setSearchResults({data: {}, error: null, loading: true})
+        const url = `https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&q=${searchText}`
+        const options = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': rapidApiKey,
+                'X-RapidAPI-Host': rapidApiHost
+            }
+        };
+    
+        try {
+            const response = await fetch(url, options)
+            const results = await response.json()
+            setSearchResults(prev => ({...prev, data: results, loading: false}))
+        } catch (error) {
+            setSearchResults(prev => ({...prev, error: error, loading: false}))
+            console.error(error)
+        }
+    }
 
     return (
         <div className='input-wrapper'>
-            <form>
-                <input type='text' placeholder='Search by ingredient or dish name' className='search-input'>
-                </input>
+            <form onSubmit={getRecipes}>
+                <input onChange={updateInput} type='text' placeholder='Search by ingredient or dish name' className='search-input' />
                 <button className='search-button' >Search</button>
             </form>
         </div>
