62bf4b0f2ad51721fa7ca0849de13810dcb0d288
GregNicholas <gregschoenberg@gmail.com>
2023-11-01 15:08:56 -0700

    move Navbar component to App.jsx so it is inside BrowserRouter, allowing us to use Links

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Navbar from './components/navbar/navbar';
 import Home from './components/home/home'
 import ExploreRecipe from './components/explore/exploreRecipe';
 import RecipeDetails from './components/recipedetails/recipeDetails';
@@ -10,6 +11,7 @@ function App() {
   return (
     <>
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route path="/explore" element={<ExploreRecipe/>} />
