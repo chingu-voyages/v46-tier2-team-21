f1edd0273dbc1b37ba3723dfd346bb8f020f3eb2
Akashay Anand <akashayanand1@gmail.com>
2023-11-11 21:00:58 +0530

    show cards in saveed List page

--- src/components/saveList/savedresults.jsx
+++ src/components/saveList/savedresults.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React,{useEffect, useState, useContext} from 'react';
+import SearchResults from '../searchresults/searchresults'
+import { RecipeContext } from '../context/savedContext';
+
 
 function Savedresults() {
+  
+  const { savedRecipe,fetchRecipe } = useContext(RecipeContext);
+    useEffect(() => {
+      fetchRecipe();
+    },[])
+  
+  const searchedRecipes = Object.values(savedRecipe);
+
+  console.log(searchedRecipes)
+
   return (
-    <div>
-      <h1> Hello World</h1>
+    <>
+        
+    <div style={{ marginTop: '20px'}}>
+    { searchedRecipes && <SearchResults searchedRecipes={searchedRecipes} /> }
+    {/* we can also style the following */}
+    { searchedRecipes.length === 0 ? ( <h2 style={{ textAlign: 'center', fontSize: '24px', color:'blue' }}>Empty list</h2> ):"" }
+
     </div>
+
+        
+    </>
   )
 }
 
