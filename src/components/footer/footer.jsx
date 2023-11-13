49091557be1861d546a312b1303e4a5b7ffd37c0
GregNicholas <gregschoenberg@gmail.com>
2023-11-12 22:15:56 -0800

    add simple footer style with github link

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,14 +1,14 @@
 import { Link } from 'react-router-dom'
+import githubLogo from '/assets/github-mark.png';
 import './footer.css'
 
 const Footer = () => {
     return (
         <footer className='footer'>
             <div className='footer-items'>
-                <h3 className='logo'>Recipe Box</h3>
                 <p className='footer-message'>
-                  Developed by Chingu Voyage 46 Team 21 
-                  <Link to='https://github.com/chingu-voyages/v46-tier2-team-21'>Github</Link>
+                  Chingu Voyage 46 Team 21: 
+                  <Link className='footer-link' to='https://github.com/chingu-voyages/v46-tier2-team-21'><img id='footer-img' src={githubLogo}></img><span>Github</span></Link>
                 </p>
             </div>
         </footer>
